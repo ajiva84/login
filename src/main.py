f5322d7700bee68ed98998442ebf79de04a4c814
ajiva84 <jivazam@gmail.com>
2021-04-05 03:17:25 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -9,6 +9,9 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +41,54 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+        
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    
+
+    if not email:
+        return jsonify({"msg": "Missing email paramter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password paramter"}), 400
+
+    try:
+        user = User.query.filter_by(email=email).first()
+        if user.validate(password):
+            db.session.commit()
+            expires = datetime.timedelta(days=7)
+            response_msg = {
+                "user":user.serialize(),
+            }
+            status_code = 200
+        else:
+            raise Exception("Failed to login. Check your email and password.")
+
+    except Exception as e:
+        response_msg = {
+            "msg": str(e),
+            "status": 401
+        }
+        status_code = 401
+
+
+    return jsonify(response_msg), status_code
+
+@app.route("/protected", methods=["GET"])
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify( {
+        
+        "logged_in_as": current_user,
+        "msg": "Access Granted to protected route"
+        }), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,15 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(256), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
+
+    def validate(self, password):
+        if not check_password_hash(self.password,password):
+            return False
+        
+        return True
 
     def serialize(self):
         return {
